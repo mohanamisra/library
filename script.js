a5993a5e40dcae638965abd0cd6e34f32f309a88
Mohana Misra <iammohanamisra@gmail.com>
2023-06-23 10:27:24 +0530

    Form open/close functionality works

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let container = document.body; // Fastest way to select the body
 let addBookButton = document.querySelector('#add-book');
 
 let form = document.querySelector('form');
+let others = document.querySelectorAll('body >*:not(.form)');
 let addButton = document.querySelector('#add');
 
 let books = [];
@@ -38,8 +39,6 @@ function makeCard(book) {
 }
 
 function readInput() {
-    form.classList.toggle('inactive');
-    document.querySelectorAll('body >*:not(.form)').forEach(e => e.classList.remove('blur'));
     let title = document.getElementById('book-title').value;
     let author = document.getElementById('book-author').value;
     let pages = document.getElementById('book-pages').value;
@@ -48,10 +47,32 @@ function readInput() {
     makeCard(books.pop());
 }
 
+function closeForm(event) {
+    if(!form.contains(event.target)) {
+        form.classList.add('inactive');
+        others.forEach(e => e.classList.remove('blur'));
+        document.removeEventListener('click', closeForm);
+    }
+}
+
+function openForm() {
+    form.classList.remove('inactive');
+    others.forEach(e => e.classList.add('blur'));
+    document.addEventListener('click', closeForm, true);
+}
+
 //POP UP form mechanism
-addBookButton.addEventListener('click', () => {
-    form.classList.toggle('inactive');
-    document.querySelectorAll('body >*:not(.form)').forEach(e => e.classList.add('blur'));
-});
+addBookButton.addEventListener('click', function(event) {
+    openForm();
+    event.stopPropagation(); // Prevent event propagation to the document
+  });
+  
+    // form.classList.toggle('inactive');
+    // document.querySelectorAll('body >*:not(.form)').forEach(e => e.classList.add('blur'));
+    // others.forEach(e => e.classList.toggle('blur'));
+    // others.forEach(e => e.addEventListener('click', () => {
+    //     console.log('triggered');
+    //     form.classList.add('inactive');
+    // }));
 
 addButton.addEventListener('click', readInput);
